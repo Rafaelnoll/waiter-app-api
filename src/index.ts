c9e6eae6525e0a05ce0044ec98f973edc37843a2
Rafael Noll <rafanoll2004@gmail.com>
2022-11-15 11:06:18 -0300

    Adding mongoose and the models(Category,Order,Product)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+
 const app = express();
 const PORT = 3001;
 
-app.listen(PORT, () => {
-	console.log(`🚀 Server is running on http://localhost:${PORT}`);
+mongoose.connect("mongodb://localhost:27017")
+	.then(() => {
+		app.emit("ready");
+		console.log("Connected with mongo");
+	})
+	.catch(() => console.log("Error to connect with mongo"));
+
+
+app.on("ready", () => {
+	app.listen(PORT, () => {
+		console.log(`🚀 Server is running on http://localhost:${PORT}`);
+	});
 });
