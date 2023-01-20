b5ce1ee363a0f57e40f5ce48aed8a095590f6790
Rafael Noll <rafanoll2004@gmail.com>
2023-01-20 17:46:11 -0300

    Fix: Setting the rigth cors access controls

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,8 @@ app.on("ready", () => {
 
 	app.use((req, res, next) => {
 		res.setHeader("Access-Control-Allow-Origin", "*");
-		res.setHeader("Access-Control-Allow-Origin", "*");
-		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Headers", "*");
+		res.setHeader("Access-Control-Allow-Methods", "*");
 		next();
 	});
 	app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
