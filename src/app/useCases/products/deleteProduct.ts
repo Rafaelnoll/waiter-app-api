5d58814f098fe3cd9f36522d72f943ca2e611a58
Rafael Noll <rafanoll2004@gmail.com>
2023-02-15 16:23:48 -0300

    Feature: When a product is removed his image from uploads folder will be deleted

--- src/app/useCases/products/deleteProduct.ts
+++ src/app/useCases/products/deleteProduct.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { io } from "../../..";
 import { Product } from "../../models/Product";
+import fs from "fs";
+import path from "node:path";
 
 export async function deleteProduct(req: Request, res: Response) {
 	const { id } = req.params;
@@ -8,8 +10,19 @@ export async function deleteProduct(req: Request, res: Response) {
 	try {
 		const product = await Product.findByIdAndDelete(id);
 
+		if (!product) {
+			res.status(404);
+			res.json({ msg: "Product not found!" });
+			return;
+		}
+
+		const pathToImage = path.resolve(__dirname, "..", "..", "..", "..", "uploads", product?.imagePath);
+		// eslint-disable-next-line @typescript-eslint/no-empty-function
+		fs.unlink(pathToImage, () => { });
+
 		io.emit("product@deleted", id);
-		res.json(product);
+		res.status(200);
+		res.json({ msg: "Product deleted" });
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({
